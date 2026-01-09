d365c1459a4d7ad511593e7926adf18ace7dbf55
Ronak-IIITD <ronak24482@iiitd.ac.in>
2026-01-09 17:37:26 +0530

    echo implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <cstddef>
+#include <filesystem>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 int main() {
@@ -13,11 +15,60 @@ int main() {
 
   while (std::getline(std::cin, command)) {
 
-    if (command == "exit") {
-      return 0;
+    if (command.empty()) {
+      std::cout << "$ ";
+      continue;
     }
 
-    std::cout << command << ": command not found" << std::endl;
+    std::stringstream ss(command);
+    std::string command_name;
+
+    // extract the first word
+    ss >> command_name;
+
+    if (command_name == "exit") {
+      return 0;
+    } else if (command_name == "echo") {
+      std::string remaining;
+      // get the rest of the line
+      std::getline(ss, remaining);
+
+      // Trim the leading space that getline picks up
+      // (operator>> skips whitespace, but getline reads from the very next
+      // character)
+      if (!remaining.empty() && remaining[0] == ' ') {
+        remaining = remaining.substr(1);
+      }
+      std::cout << remaining << std::endl;
+    } else {
+      // this only runs if the command was not as what we get in input is not
+      // valid command
+      std::cout << command << ": command not found" << std::endl;
+    }
+
     std::cout << "$ ";
+
+    //   if (command == "exit") {
+    //     return 0;
+    //   }
+
+    //   std::stringstream ss(command);
+    //   std::string word;
+    //   while (ss >> word) {
+    //     if (word == "echo") {
+    //       std::string remaining;
+    //       std::getline(ss, remaining);
+
+    //       if (!remaining.empty() && remaining[0] == ' ') {
+    //         remaining = remaining.substr(1);
+    //       }
+    //       std::cout << remaining << std::endl;
+    //       break;
+    //     }
+    //   }
+
+    //   std::cout << command << ": command not found" << std::endl;
+    //   std::cout << "$ ";
+    // }
   }
 }
